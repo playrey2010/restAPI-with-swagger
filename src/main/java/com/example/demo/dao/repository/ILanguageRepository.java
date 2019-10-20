9b5a39628ee074cf0b4985e939f47da188db3086
playrey2010 <playrey2010@gmail.com>
2019-10-20 18:56:01 -0400

    first part done

--- src/main/java/com/example/demo/dao/repository/ILanguageRepository.java
+++ src/main/java/com/example/demo/dao/repository/ILanguageRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.dao.repository;
 
-public interface ILanguageRepository {
+import com.example.demo.dao.entity.Language;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ILanguageRepository  extends JpaRepository<Language, Long> {
 }
